683c8bb6369edb7cb969434a6caffa0c0b7ec1d6
Chad Harper <chadharper811@gmail.com>
2025-06-28 12:26:25 -0500

    added props to banner in app.jsx file

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,13 +1,19 @@
 import logo from "../assets/GloboLogo.png"
 import {logo as logoClass} from "./Banner.module.css"
 
-const Banner = () => {
+const subtitleStyle = {
+    fontStyle: "italic",
+    fontSize: "x-large",
+    color: "coral",
+};
+
+const Banner = ({headerText}) => {
     return (
         <header className="row mb-4">
             <div className="col-5">
                 <img src={logo} className={logoClass} alt="logo" />
             </div>
-            <div className="col-7 mt-5">Providing houses all over the world</div>
+            <div className="col-7 mt-5" style={subtitleStyle}>{headerText}</div>
         </header>
     );
 }
