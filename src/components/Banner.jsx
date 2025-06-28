f44ae8298f815090b93d6c83079d65cd1526f2fe
Chad Harper <chadharper811@gmail.com>
2025-06-28 12:11:04 -0500

    updated banner with bootstrap for sizing logo and spacing

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,11 +2,11 @@ import logo from "../assets/GloboLogo.png"
 
 const Banner = () => {
     return (
-        <header>
-            <div>
-                <img src={logo} alt="logo" />
+        <header className="row mb-4">
+            <div className="col-5">
+                <img src={logo} className="logo" alt="logo" />
             </div>
-            <div>Providing houses all over the world</div>
+            <div className="col-7 mt-5">Providing houses all over the world</div>
         </header>
     );
 }
