80adc52e9bacf1632909daea21aaf8a475122b87
Chad Harper <chadharper811@gmail.com>
2025-06-28 12:46:54 -0500

    added houselist to app.jsx and rendered table with data in houselist

--- src/components/HouseList.jsx
+++ src/components/HouseList.jsx
@@ -1,4 +1,19 @@
 
+const houses = [
+  {
+    id: 1,
+    address: "12 Valley of Kings, Geneva",
+    country: "Switzerland",
+    price: 900000,
+  },
+  {
+    id: 2,
+    address: "89 Road of Forks, Bern",
+    country: "Switzerland",
+    price: 500000,
+  },
+];
+
 const HouseList = () => {
     return (
     <>
@@ -16,6 +31,13 @@ const HouseList = () => {
           </tr>
         </thead>
         <tbody>
+            {houses.map(h => (
+                <tr key={h.id}>
+                    <td>{h.address}</td>
+                    <td>{h.country}</td>
+                    <td>{h.price}</td>
+                </tr>
+            ))}
         </tbody>
       </table>
     </>
