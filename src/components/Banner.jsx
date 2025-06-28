d306da20f08fc4ea555b8557fdcdaeaf98a2d57b
Chad Harper <chadharper811@gmail.com>
2025-06-28 12:14:29 -0500

    new file for banner css, moved logo styling to new file

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,10 +1,11 @@
 import logo from "../assets/GloboLogo.png"
+import {logo as logoClass} from "./Banner.module.css"
 
 const Banner = () => {
     return (
         <header className="row mb-4">
             <div className="col-5">
-                <img src={logo} className="logo" alt="logo" />
+                <img src={logo} className={logoClass} alt="logo" />
             </div>
             <div className="col-7 mt-5">Providing houses all over the world</div>
         </header>
