19093811fb27d59d82594c485682c40291cbcf49
Chad Harper <chadharper811@gmail.com>
2025-06-29 16:19:28 -0500

    added text color to only house prices > 500,000

--- src/components/HouseRow.jsx
+++ src/components/HouseRow.jsx
@@ -1,11 +1,18 @@
 import currenyFormatter from "../helpers/currenyFormatter";
 
 const HouseRow = ({house}) => {
+    let priceTd;
+    if(house.price < 500000)
+        priceTd = <td>{currenyFormatter.format(house.price)}</td>;
+    else
+        priceTd = (
+            <td className="text-primary">{currenyFormatter.format(house.price)}</td>
+        );
     return (
         <tr>
             <td>{house.address}</td>
             <td>{house.country}</td>
-            <td>{currenyFormatter.format(house.price)}</td>
+            {priceTd}
         </tr>
     );
 }
