055506139b4dd4368036ec44f01d00a654152a35
Chad Harper <chadharper811@gmail.com>
2025-06-29 16:22:08 -0500

    changed if statement for text color change to expression template literal to clean code

--- src/components/HouseRow.jsx
+++ src/components/HouseRow.jsx
@@ -1,18 +1,13 @@
 import currenyFormatter from "../helpers/currenyFormatter";
 
 const HouseRow = ({house}) => {
-    let priceTd;
-    if(house.price < 500000)
-        priceTd = <td>{currenyFormatter.format(house.price)}</td>;
-    else
-        priceTd = (
-            <td className="text-primary">{currenyFormatter.format(house.price)}</td>
-        );
     return (
         <tr>
             <td>{house.address}</td>
             <td>{house.country}</td>
-            {priceTd}
+            <td className={`${house.price >= 500000 ? "text-primary" : ""}`}>
+                {currenyFormatter.format(house.price)}
+            </td>
         </tr>
     );
 }
